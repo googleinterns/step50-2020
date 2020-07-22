c2fe217bfe27eb59ef987c6f82fffe9fde642212
Grace Luo <graceluo@berkeley.edu>
2020-07-22 20:17:15 +0000

    Create initial folder nesting functions

--- project/src/main/java/com/google/sps/Models/Folder.java
+++ project/src/main/java/com/google/sps/Models/Folder.java
@@ -8,6 +8,7 @@ public class Folder {
   ArrayList<String> docHashes;
   ArrayList<Document> docs;
   ArrayList<Long> folderIDs;
+  ArrayList<Folder> folders;
   
   Folder(String name, long folderID, ArrayList<String> docHashes, ArrayList<Long> folderIDs) {
     this.name = name;
@@ -38,5 +39,13 @@ public class Folder {
 
   public ArrayList<Document> getDocs() {
     return docs;
+  }
+
+  public void setFolders(ArrayList<Folder> folders) {
+    this.folders = folders;
+  }
+
+  public ArrayList<Folder> getFolders() {
+    return folders;
   }
 }
